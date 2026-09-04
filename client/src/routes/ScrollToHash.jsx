aed168af1e9535138ad5db19dd59100cbcc69611
ismailarifberlianto <ismail.aberlianto@gmail.com>
2026-09-04 21:01:51 +0700

    fix: Scroll smooth

--- client/src/routes/ScrollToHash.jsx
+++ client/src/routes/ScrollToHash.jsx
@@ -1,20 +1,22 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 
-
 function ScrollToHash() {
   const location = useLocation();
 
   useEffect(() => {
     if (location.hash) {
-      const el = document.querySelector(location.hash);
+      const id = location.hash.replace("#", "");
+      const el = document.getElementById(id);
       if (el) {
-        el.scrollIntoView({ behavior: "smooth" });
-        return;
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 0);
       }
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
-    window.scrollTo({ top: 0 });
-  }, [location.pathname, location.hash]);
+  }, [location]);
 
   return null;
 }
